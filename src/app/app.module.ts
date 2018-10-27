ee4b4dde1d5306446aca6a8f76bb19e4370efa10
gwfernandez <gwfernandez@gmail.com>
2018-10-27 13:03:11 -0300

    preparar form para usar servicios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
 import { HeroeComponent } from './components/heroes/heroe.component';
 import { AppRoutingModule } from './app.routes';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { HeroesService } from './services/heroes.service';
 
 @NgModule({
   declarations: [
@@ -16,9 +18,10 @@ import { NavbarComponent } from './components/navbar/navbar.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [HeroesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/heroes/heroe.component.ts
+++ src/app/components/heroes/heroe.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { NgForm } from "@angular/forms";
+import { Heroe } from "../../interfaces/heroe.interface";
 
 @Component({
-  selector: 'app-heroe',
-  templateUrl: './heroe.component.html',
+  selector: "app-heroe",
+  templateUrl: "./heroe.component.html",
   styles: []
 })
 export class HeroeComponent implements OnInit {
+  public heroe: Heroe;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    this.heroe = {
+      nombre: "",
+      casa: "MARVEL",
+      bio: ""
+    };
   }
 
+  ngOnInit() {}
+
+  public guardar() {
+    console.log(this.heroe);
+    
+  }
 }
