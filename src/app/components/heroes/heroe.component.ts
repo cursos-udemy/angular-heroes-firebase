569bfd0025ae745a794f668912b291316e51f8d2
gwfernandez <gwfernandez@gmail.com>
2018-10-27 16:25:49 -0300

    actualizando heroe en firebase

--- src/app/components/heroes/heroe.component.ts
+++ src/app/components/heroes/heroe.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { NgForm } from "@angular/forms";
 import { Heroe } from "../../interfaces/heroe.interface";
 import { HeroesService } from "../../services/heroes.service";
-import { Router } from "@angular/router";
+import { Router, ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: "app-heroe",
@@ -11,8 +11,20 @@ import { Router } from "@angular/router";
 })
 export class HeroeComponent implements OnInit {
   public heroe: Heroe;
+  public nuevo: boolean = false;
+  public id: string;
 
-  constructor(private heroesService: HeroesService, private router: Router) {
+  constructor(
+    private heroesService: HeroesService,
+    private router: Router,
+    private activatedRouted: ActivatedRoute
+  ) {
+    activatedRouted.params.subscribe(params => {
+      console.log("Params: ", params);
+      this.id = params["id"];
+      this.nuevo = this.id == "nuevo";
+    });
+
     // inicializo la variable heroe
     this.heroe = {
       nombre: "",
@@ -26,14 +38,26 @@ export class HeroeComponent implements OnInit {
   public guardar() {
     console.log(this.heroe);
 
-    this.heroesService.nuevoHeroe(this.heroe).subscribe(
-      (data: any) => {
-        console.log("servicio.guardar(): ", data);
-        this.router.navigate(["/heroe", data.name]);
-      },
-      error => {
-        console.error("Error al guardar: ", error);
-      }
-    );
+    if (this.nuevo) {
+      this.heroesService.nuevoHeroe(this.heroe).subscribe(
+        (data: any) => {
+          console.log("servicio.guardar(): ", data);
+          this.router.navigate(["/heroe", data.name]);
+        },
+        error => {
+          console.error("Error al guardar: ", error);
+        }
+      );
+    } else {
+      // this.heroe.key$ = this.id;
+      this.heroesService.actualizarHeroe(this.heroe, this.id).subscribe(
+        (data: any) => {
+          console.log("servicio.guardar(): ", data);
+        },
+        error => {
+          console.error("Error al guardar: ", error);
+        }
+      );
+    }
   }
 }
--- src/app/services/heroes.service.ts
+++ src/app/services/heroes.service.ts
@@ -3,8 +3,9 @@ import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Heroe } from "../interfaces/heroe.interface";
 // import "rxjs/Rx";
 
-const FIREBASE_URL: string =
-  "https://heroesapp-3ff73.firebaseio.com/heroes.json";
+const FIREBASE_URL: string = "https://heroesapp-3ff73.firebaseio.com";
+const FIREBASE_DATABASE: string = FIREBASE_URL + "/heroes";
+const FIREBASE_DATABASE_JSON: string = FIREBASE_URL + "/heroes.json";
 
 @Injectable({
   providedIn: "root"
@@ -12,16 +13,29 @@ const FIREBASE_URL: string =
 export class HeroesService {
   constructor(private http: HttpClient) {
     console.log("HeroesService init [OK]");
-    console.log(FIREBASE_URL);
   }
 
   public nuevoHeroe(heroe: Heroe) {
     let body = JSON.stringify(heroe);
-    let headers = new HttpHeaders ({
+    let headers = new HttpHeaders({
       "Content-Type": "application/json"
     });
 
-    return this.http.post (FIREBASE_URL, body, { headers: headers });
+    return this.http.post(FIREBASE_DATABASE_JSON, body, { headers: headers });
+    // map(r => {
+    //   console.log(r.json());
+    //   return r.json();
+    // });
+  }
+
+  public actualizarHeroe(heroe: Heroe, key$:string) {
+    let body = JSON.stringify(heroe);
+    let headers = new HttpHeaders({
+      "Content-Type": "application/json"
+    });
+    let url = `${FIREBASE_DATABASE}/${key$}.json`;
+
+    return this.http.put(url, body, { headers: headers });
     // map(r => {
     //   console.log(r.json());
     //   return r.json();
